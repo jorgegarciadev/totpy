1155a5a0678d6d1797c296e69b8b00be260383e0
Jorge García <db0035@icloud.com>
2020-03-14 03:05:26 +0100

    0.3.2

--- setup.py
+++ setup.py
@@ -5,7 +5,7 @@ def description():
 
 setup(
     name='totpy',
-    version='0.3.1',
+    version='0.3.2',
     author='Jorge García',
     author_email='jorgegarciadev@icloud.com',
     description='Totpy - CLI TOTP generator and management tool',
